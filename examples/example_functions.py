98758a41f135b2db1f87df78998f9383b2d77d8a
Justin Johnson <jajohns@umn.edu>
2024-04-01 08:49:08 -0500

    docs for skill session

---
+++ examples/example_functions.py
@@ -0,0 +1,6 @@
+import os
+import hazelbean as hb
+
+def execute_water_yield(args):
+    description = """Actual execution of the invest water yield model"""
+    
---
+++ examples/example_tasks.py
@@ -0,0 +1,21 @@
+import os, sys
+import hazelbean as hb
+import seals.seals_utils as seals_utils
+import pandas as pd
+import global_invest
+import numpy as np
+from global_invest import ecosystem_services_functions
+import dask
+# from seals import seals_utils
+
+def ecosystem_services(p):
+    pass # Just to generate a folder
+
+def water_yield(p):
+    description = """Calculate water yield LULC maps."""
+    print(description)
+
+def another_example(p):
+    description = """It wouldn't be much of a tree if it didn't branch..."""
+    print(description)
+
